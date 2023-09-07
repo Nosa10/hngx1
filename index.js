b58118e4816340ccee9d225c1492e77b75242bec
Nosa10 <edisonnosakhare@gmail.com>
2023-09-07 07:10:55 -0700

    Changes

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-var myDate = new Date();
-let day = myDate.getDay()
-let ndate = myDate.toLocaleString();
-var days = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', ];
-let dayss = days[day];
-const hours = myDate.getUTCHours();
-const minutes = myDate.getUTCMinutes();
-const seconds = myDate.getUTCSeconds();
-const milliseconds = myDate.getUTCMilliseconds();
-const time = (`${hours}: ${minutes}: ${seconds}. ${milliseconds}`)
-console.log(time)
-document.getElementById('utcTime').innerHTML = time;
-document.getElementById('dayOfWeek').innerHTML= dayss;
+var times = document.getElementById('utcTime');
+var daye = document.getElementById('dayOfWeek');
+//console.log(time)
+function currentTime() {
+    var myDate = new Date();
+    let day = myDate.getDay()
+    let ndate = myDate.toLocaleString();
+    var days = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', ];
+    let dayss = days[day];
+    const hours = myDate.getUTCHours();
+    const minutes = myDate.getUTCMinutes();
+    const seconds = myDate.getUTCSeconds();
+    const milliseconds = myDate.getUTCMilliseconds();
+    const time = (` ${milliseconds}`) 
+    times.innerHTML = 'Current UTC Time in millisecond : '+ time;
+    daye.innerHTML = 'Current day of the week : ' +dayss;
+}
+setInterval(currentTime, 1000)
+// a.innerHTML = time;
+// b.innerHTML= dayss;
